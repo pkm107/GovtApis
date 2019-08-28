5a320484912f836ae6d09032faedd5d6c56da2aa
Piyusha Kumar mohanty <pkm.107@gmail.com>
2019-08-28 16:29:09 +0530

    Updated for Heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+
+app.use(express.static('/app/dist/MyProj'));
+
+// Start the app by listening on the default
+// Heroku port
+const path = require('path');
+
+// ...
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('/app/dist/GovtApis/index.html'));
+});
+app.listen(process.env.PORT || 8080);
