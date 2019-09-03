326e274c938e6803013a8c9f030fa41345a3728c
Piyusha Kumar mohanty <pkm.107@gmail.com>
2019-09-03 18:01:49 +0530

    For error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static('/app/dist/MyProj'));
+app.use(express.static('/app/dist/GovtApis'));
 
 // Start the app by listening on the default
 // Heroku port
